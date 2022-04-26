c6cad7173bfb74cfbfd27528f185a5dee6a72aac
berm0427 <berm0427@naver.com>
2022-04-26 13:36:59 +0900

    Update 포인터2.c

--- repos/Project3/Project3/포인터2.c
+++ repos/Project3/Project3/포인터2.c
@@ -1,15 +1,17 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int* ptr = NULL;
-	int num = 1;
-	ptr = &num;
-	printf("[>] num value : %d\n", num);
-	*ptr = 10;
-	printf("[>] num value : %d\n", num);
-	*ptr = 20;
-	printf("[>] num value : %d\n", num);
+    int* ptr = NULL; //포인터 선언문
+    int num = 1; // 정수형 변수 선언문
 
-	return 0;
+    ptr = &num; // 포인터에 num변수의 주소를 저장
+    printf("[>] num value : %d\n", num); //num변수에 저장된 값을 출력
+
+    *ptr = 10; // 포인터를 이용해 num값을 바꾸는 구문
+    printf("[>] num value : %d\n", num);// 바뀐 num값을 출력
+
+    *ptr = 20; // 포인터를 이용해 num값을 바꾸는 구문
+    printf("[>] num value : %d\n", num); // 바뀐 num값을 출력
+    return 0; 
 }
