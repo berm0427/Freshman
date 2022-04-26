4383b3989ed76b4f3ccf31bab27c60c3ce2893ed
berm0427 <berm0427@naver.com>
2022-04-26 13:50:57 +0900

    Update 포인터4.c

--- repos/Project3/Project3/포인터4.c
+++ repos/Project3/Project3/포인터4.c
@@ -1,11 +1,11 @@
-# include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
-	int num[4] = { 10, 20, 30, 40 };
+    int num[4] = { 10, 20, 30, 40 }; // 정수형 배열 선언
 
-	printf("[>] *(num + 2) value : %d\n", *(num + 2));
-	printf("[>] *num + 2 value : %d\n", *num + 2);
+    printf("[>] *(num + 2) value : %d\n", *(num + 2)); // num[2]의 값 출력
+    printf("[>] *num + 2 value : %d\n", *num + 2); // num[1] + 2의 값을 출력
 
-	return 0;
+    return 0;
 }
