213d0572d42a5072daed7fe9e9dadc4b65585ecb
berm0427 <berm0427@naver.com>
2022-04-29 19:02:38 +0900

    Create hap.c

---
+++ repos/Project4/Project4/hap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void hap(int su1,int su2)
+{
+	int sum = 0;
+	for (int i=su1; i<=su2;i++)
+	{
+		sum += i;
+	}
+	printf("%d에서 %d까지의 합 : %d\n",su1,su2,sum);
+}
+
+int main()
+{
+	hap(1, 10);
+	hap(1, 50);
+}
