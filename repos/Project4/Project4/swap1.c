fc9bb1e6f0a47eff11a80ec82b3415dc3f7833c2
berm0427 <berm0427@naver.com>
2022-05-01 14:11:26 +0900

    Create swap1.c

---
+++ repos/Project4/Project4/swap1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+	printf("a : %d \n", a);
+	printf("b : %d \n", b);
+}
+int main()
+{
+	int val1 = 10;
+	int val2 = 20;
+	swap(val1, val2);
+
+	printf("val1 : %d \n", val1);
+	printf("val2 : %d \n", val2);
+	return 0;
+}
