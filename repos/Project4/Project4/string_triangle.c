b132c176725b021a57d0b1a4b5fb72bc4a4056a9
berm0427 <berm0427@naver.com>
2022-05-02 22:03:20 +0900

    Update string_triangle.c

--- repos/Project4/Project4/string_triangle.c
+++ repos/Project4/Project4/string_triangle.c
@@ -2,12 +2,12 @@
 
 int main()
 {
-    char str[] = "Hello World";
-    char* ptr = str;
-    while (*ptr)
+    char str[] = "Hello World"; //배열 선언
+    char* ptr = str; // 포인터 선언
+    while (*ptr) // 포인터가 가리키는 값이 '\0'이 아닐 때 까지 반복
     {
-        printf("%s\n", ptr);
-        ptr++;
+        printf("%s\n", ptr); // 포인터가 n번째를 가리키면 n번째부터 모두 출력
+        ptr++; // 포인터가 다음 위치로 이동
     }
-    return 0;
+    return 0; // 리턴
 }
