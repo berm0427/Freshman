20f03a214cb60013a24de423bbea3555a37d02d4
berm0427 <berm0427@naver.com>
2022-05-06 19:23:26 +0900

    Update my_book.c

--- repos/Project4/Project4/my_book.c
+++ repos/Project4/Project4/my_book.c
@@ -12,7 +12,7 @@ typedef struct book
 int main()
 {
 	my_book my;
-	strcpy(my.title,"snowman"); // C++에서는 별도의 선언이 필요없지만, C언어는 "#include <cstring>"라는 선언이 필요함.
+	strcpy(my.title,"snowman"); // C에서는 별도의 선언이 필요없지만, C++에서는 "#include <cstring>"라는 선언이 필요함.
 	my.price = 20000;
 	printf("[>] title : %s\t price : %d\t", my.title, my.price);
 	return 0;
