8e5c6ffc45a7c34b1afe24014099c22485e01122
berm0427 <berm0427@naver.com>
2022-05-06 11:58:55 +0900

    Create gugu.c

---
+++ repos/Project4/Project4/gugu.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+# include <stdio.h>
+
+int main()
+{
+	for (int i = 2; i < 10; i++)
+	{
+		printf("[>] %d단\n", i);
+		for (int j = 1; j < 10; j++)
+		{
+			printf("[>] %d X %d = %d\n", i, j, i * j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
