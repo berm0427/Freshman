87739d32748163577589acd4c9520d32dff17620
berm0427 <berm0427@naver.com>
2022-05-10 03:47:36 +0900

    Create file_writer_with_struct(완전판).c

---
+++ repos/Project4/Project4/file_writer_with_struct(완전판).c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+struct person
+{
+	char name[10];
+	int age;
+	char gender;
+};
+int main()
+{
+	char name[10] = { '\0', };
+	int age = 0;
+	char gender;
+	struct person p = { '\0', };
+	struct person* ptr = &p;
+	printf("[>] 이름 \n[>] ");
+	scanf("%s", name);
+	printf("[>] 나이 \n[>] ");
+	scanf("%d", &age);
+	printf("[>] 성별 \n[>] ");
+	scanf("\n%c", &gender);
+	strcpy(p.name,name);
+	p.age = age;
+	p.gender = gender;
+	FILE* fp;
+	fp = fopen("data.txt", "w+");
+	fprintf(fp, "%s %d %c", p.name, p.age, p.gender);
+	return 0;
+	
+}
