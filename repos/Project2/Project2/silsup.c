d897a36579f662ef684a60fe2e9c9ea2601f962d
berm0427 <berm0427@naver.com>
2023-03-12 23:01:48 +0900

    Update silsup.c

--- repos/Project2/Project2/silsup.c
+++ repos/Project2/Project2/silsup.c
@@ -7,6 +7,6 @@ int main()
 	scanf("%d", &num1);
 	printf("input num2 : ");
 	scanf("%d", &num2);
-	num1 > num2 ? printf("¸ò : %d\n³ª¸ÓÁö : %d\n", num1 / num2, num1 % num2) : printf("¸ò : %d\n³ª¸ÓÁö : %d\n", num2 / num1, num2 % num1);
+	num1 > num2 ? printf(" ëª« : %d\n ë‚˜ë¨¸ì§€ : %d\n", num1 / num2, num1 % num2) : printf(" ëª«: %d\n ë‚˜ë¨¸ì§€ : %d\n", num2 / num1, num2 % num1);
 	return 0;
 }
